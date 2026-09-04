d1d171838a918df6252b75743af8b76c7b36644c
kevo13-web <kevorkbanoyan@gmail.com>
2026-09-04 11:51:31 +0200

    Soften cards with cream blue styling

--- components/GlassCard.tsx
+++ components/GlassCard.tsx
@@ -9,7 +9,7 @@ export function GlassCard({ children, className }: GlassCardProps) {
   return (
     <div
       className={cn(
-        "glass rounded-lg p-5 transition duration-300 hover:-translate-y-1 hover:border-mint/30",
+        "glass rounded-2xl p-5 transition duration-300 hover:-translate-y-1 hover:border-[#5d8cb3]/30 hover:shadow-[0_22px_60px_rgba(55,85,110,0.13)]",
         className,
       )}
     >
